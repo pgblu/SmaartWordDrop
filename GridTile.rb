a8e0c6ca5caa4f22a3044843217eec06b4747386
Philipp Blume <pgbluster@gmail.com>
2018-04-29 18:05:24 -0500

    new methods for GridTile

--- GridTile.rb
+++ GridTile.rb
@@ -1,16 +1,37 @@
-# column, position, letter, position_history = [], empty?, visited?
+# h_position, letter, vposition_history = [], empty?, visited?
 
 class GridTile
-  attr_accessor :column, :position_history, :letter, :visited?
+  attr_accessor :h_position, :v_history, :letter, :visited?, :grid
 
-  def initialize (column, position_history, letter, visited)
-    @column = column
-    @position_history = position_history
+  def initialize (h_position, v_history, letter, visited?)
+    @h_position = h_position
+    @v_history = v_history
     @letter = letter
-    @visited = visited
+    @visited? = visited?
+    @grid = grid
+  end
+
+  def v_position
+    v_history[-1]
+  end
+
+  def neighbors
+    result = grid.select do |other|
+      (other.h_position - h_position).abs <= 1
+      && (other.v_position - cell.v_position).abs <= 1 }
+    end
+    result.delete(self)
+    result
   end
 
   def empty?
     @letter == "*"
   end
-end
+
+  def available?
+    !empty? && !visited?
+  end
+
+  def available_neighbors
+    self.neighbors.select{ &:available? }
+  end
